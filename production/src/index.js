c4865a41fb2872bee5530eb956e608e9d83d692d
John Tucker <john@larkintuckerllc.com>
2017-05-12 18:25:49 -0400

    production

---
+++ production/src/index.js
@@ -0,0 +1,4 @@
+window.console.log('hello world');
+if (process.env.NODE_ENV !== 'production') {
+  window.console.log('not in production');
+}
---
+++ production/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: []
+	},
+
+	plugins: [new UglifyJSPlugin()]
+};
