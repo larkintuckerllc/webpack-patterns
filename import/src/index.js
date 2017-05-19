5aac2ddb84e7d6e2ea32ff3504a9d8c8a760a6d9
John Tucker <john@larkintuckerllc.com>
2017-05-19 06:32:53 -0400

    secondary.js

--- import/src/index.js
+++ import/src/index.js
@@ -1,19 +1,18 @@
+/*
+require('babel-polyfill');
+require('./index.css');
+var cute = require('./cute.jpg');
+*/
 import 'babel-polyfill';
 import './index.css';
 import cute from './cute.jpg';
+// DEMO EXPORT
+import secondary from './secondary';
 
 var rootEl = document.getElementById('root');
 var imageEl = document.createElement('img');
 imageEl.src = cute;
 rootEl.appendChild(imageEl);
 
-// DEMO ES2015
-let demo = 'DEMO';
-demo = 'Hello ES2015';
-window.console.log(demo);
-
-// DEMO BABEL POLYFILL
-const promise = Promise.resolve('done');
-promise.then(value => {
-  window.console.log(value);
-});
+// DEMO EXPORT
+secondary();
---
+++ import/src/secondary.js
@@ -0,0 +1,8 @@
+/*
+module.exports = function() {
+  window.console.log('hello secondary');
+};
+*/
+export default () => {
+  window.console.log('hello secondary');
+};
