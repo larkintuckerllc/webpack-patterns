804ac537e30e46bd3b418f5d12cebbcc1e1ab3a6
John Tucker <john@larkintuckerllc.com>
2017-05-19 06:15:16 -0400

    babel-polyfill

---
+++ babel-polyfill/src/index.js
@@ -0,0 +1,19 @@
+require('babel-polyfill');
+require('./index.css');
+var cute = require('./cute.jpg');
+
+var rootEl = document.getElementById('root');
+var imageEl = document.createElement('img');
+imageEl.src = cute;
+rootEl.appendChild(imageEl);
+
+// DEMO ES2015
+let demo = 'DEMO';
+demo = 'Hello ES2015';
+window.console.log(demo);
+
+// DEMO BABEL POLYFILL
+const promise = Promise.resolve('done');
+promise.then(value => {
+  window.console.log(value);
+});
