125852f33a0457138bb55bfc9bd2f063c1f9d83e
John Tucker <john@larkintuckerllc.com>
2017-05-22 09:28:00 -0400

    two more sections

---
+++ css-source-map/webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = function(env) {
+  return ({
+    devtool: env === 'production' ? 'source-map' : 'cheap-eval-source-map',
+    entry: './src/index.js',
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+        test: /\.(scss|css)$/,
+          use: ExtractTextPlugin.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ],
+            fallback: 'style-loader'
+          })
+        },
+        {
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
+        }
+      ]
+    },
+    plugins: [
+      new UglifyJSPlugin({sourceMap: true}),
+      new ExtractTextPlugin('styles.css'),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+      }),
+      new CleanWebpackPlugin(['dist'])
+    ]
+  });
+}
