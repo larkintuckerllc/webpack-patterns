7956f778e8020099a7da001a45036b9b660072f2
John  Tucker <john@larkintuckerllc.com>
2017-10-31 06:04:08 -0400

    React and More

---
+++ react-and-more/src/Secondary.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Secondary.css';
+
+function Secondary() {
+  return (
+    <div id={styles.root}>Hello World</div>
+  );
+}
+export default Secondary;
---
+++ react-and-more/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Secondary from './Secondary';
+
+ReactDOM.render(<Secondary />, document.getElementById('root'));
